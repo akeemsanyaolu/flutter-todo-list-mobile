bd09ffdc1dadce2e24bc02387b2bfe8204740820
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-08-07 21:21:55 +0100

    commit

---
+++ lib/features/tasks/ui/update_task_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class UpdateTaskPage extends StatefulWidget {
+  const UpdateTaskPage({super.key});
+
+  @override
+  State<UpdateTaskPage> createState() => _UpdateTaskPageState();
+}
+
+class _UpdateTaskPageState extends State<UpdateTaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
