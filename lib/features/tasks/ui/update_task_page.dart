1135399cfca7a0c85e21f18fcb42babbb59ad8a8
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-08-07 21:23:12 +0100

    commit

--- lib/features/tasks/ui/update_task_page.dart
+++ lib/features/tasks/ui/update_task_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_todo_list_app/features/tasks/bloc/tasks_bloc.dart';
 
 class UpdateTaskPage extends StatefulWidget {
   const UpdateTaskPage({super.key});
@@ -13,6 +15,13 @@ class _UpdateTaskPageState extends State<UpdateTaskPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              context.read<TasksBloc>().add(TasksInitialFetchEvent());
+            },
+            icon: const Icon(Icons.arrow_back_ios)),
+      ),
       body: Column(),
     );
   }
