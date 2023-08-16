2184cb114a1d3f0eb356230b90f2393b6458d567
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-08-16 23:56:53 +0100

    commit

--- lib/features/auth/ui/login_page.dart
+++ lib/features/auth/ui/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -11,6 +12,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
